2cb0a653bb01da7be800bfe1749e297c343f8f5c
Lucas Aquiles <lucasaquiles@gmail.com>
2016-11-12 22:24:06 -0300

    font customizada

--- app/src/main/java/br/com/lucasaquiles/cogg/adapter/GridViewAdapter.java
+++ app/src/main/java/br/com/lucasaquiles/cogg/adapter/GridViewAdapter.java
@@ -7,11 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import br.com.lucasaquiles.cogg.R;
+import br.com.lucasaquiles.cogg.view.CustomTextView;
 import br.com.lucasaquiles.cogg.view.ImageItem;
 
 /**
@@ -38,7 +38,7 @@ public class GridViewAdapter extends ArrayAdapter {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
             row = inflater.inflate(layoutResourceId, parent, false);
             holder = new ViewHolder();
-            holder.imageTitle = (TextView) row.findViewById(R.id.text);
+            holder.imageTitle = (CustomTextView ) row.findViewById(R.id.text);
             holder.image = (ImageView) row.findViewById(R.id.image);
             row.setTag(holder);
         } else {
@@ -47,12 +47,15 @@ public class GridViewAdapter extends ArrayAdapter {
 
         ImageItem item = (ImageItem) data.get(position);
         holder.imageTitle.setText(item.getTitle());
+
+
+
         holder.image.setImageBitmap(item.getImage());
         return row;
     }
 
     static class ViewHolder {
-        TextView imageTitle;
+        CustomTextView  imageTitle;
         ImageView image;
     }
 
---
+++ app/src/main/java/br/com/lucasaquiles/cogg/view/CustomTextView.java
@@ -0,0 +1,30 @@
+package br.com.lucasaquiles.cogg.view;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by lucasaquiles on 12/11/16.
+ */
+
+public class CustomTextView extends TextView {
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/cherl.tff"));
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
